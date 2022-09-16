844a7ebc8babd9132a79898fddf0d655b8be51fe
quag-cactus <49008834+quag-cactus@users.noreply.github.com>
2022-09-16 12:44:45 +0900

    update: Update demo script

--- demo_editor.py
+++ demo_editor.py
@@ -2,20 +2,41 @@
 
 """
 
+import sys
 import time
+import signal
 import random
 import uuid
 import datetime
 
-from ipmmap import DataStructMmapEditor
+from ipmmap import DataStructMmapManager, DataStructMmapEditor
 
 def Main():
 
+    # SIGNAL HANDLER for closing memory mapping when the process interrupted.
+    def handler(signum, frame):
+        print("process intrrupted: {}".format(signum))
+        manager.closeMemory()
+        print("mmap released")
+        sys.exit(1)
+
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
+
+    # 1. resgist your IPMMAP Structure module-path.
     DataStructMmapEditor.setUserStructs(["demo_struct"])
 
-    # edit mmap file
+    # 2. call manager class for your IPMMAP Structure. 
+    manager = DataStructMmapManager("DemoMmapStructure", create=True, force=True)
+
+    # 3. map the ipmmap file to memory.
+    manager.openMemory()
+    print("memory mapped: ", manager.mmapFilePath)
+
+    # 4. edit mmap file.
     while True:
 
+        t = time.perf_counter()
         with DataStructMmapEditor("DemoMmapStructure") as editor:
 
             data_int = random.randint(0, 100)
@@ -23,29 +44,23 @@ def Main():
             x = random.random() * 100
             y = random.random() * 100
             
-            # edit 
+            # edit value of your IPMMAP Stracture 
             editor.writeData("data_int", data_int)
             editor.writeData("data_string", bytes(data_string, 'utf-8'))
+            editor.writeData("data_xy.x", x)
+            editor.writeData("data_xy.x", y)
 
-            # edit mmap buffer directly
-            buf = editor.referMappedBuffer()
-            buf.data_xy.x = x
-            buf.data_xy.y = y
-
-            # you need release reference of buffer
-            buf = None          
-
+            # update last-update time
             editor.updateLastUpdate()
 
-            print("----------------------------------")
-            print("[lastupdate] {}".format(datetime.datetime.fromtimestamp(editor.getLastUpdate())))
+            print("------INSERT TO SHARED MEM------")
+            print("[last update] {}".format(datetime.datetime.fromtimestamp(editor.getLastUpdate())))
             print("[data_int] {}".format(data_int))
             print("[data_string] {}".format(data_string))
             print("[data_xy] x: {}, y: {}".format(x, y))
-            print("updated.")
 
+        print("performance: {:.4f} ms".format((time.perf_counter() -t) * 1000))
         time.sleep(1)
-
 
 if __name__ == "__main__":
     Main()
--- demo_reader.py
+++ demo_reader.py
@@ -12,23 +12,31 @@ def Main():
     DataStructMmapReader.setUserStructs(["demo_struct"])
 
     # read mmap file
+    lastestUpdateTime = 0
     while True:
-
+        
         with DataStructMmapReader("DemoMmapStructure") as reader:
-            lastUpdateTime = datetime.datetime.fromtimestamp(reader.getLastUpdate())
 
-            data_int = reader.readData('data_int')
-            data_string = reader.readData('data_string')
-            pts_x = reader.readData('data_xy.x')
-            pts_y = reader.readData('data_xy.y')
+            updateTime = reader.getLastUpdate()
 
-            print("----------------------------------")
-            print("[last update] {}".format(lastUpdateTime))
-            print("[data_int] {}".format(data_int))
-            print("[data_string] {}".format(data_string))
-            print("[data_xy] x: {}, y: {}".format(pts_x, pts_y))
+            if updateTime > lastestUpdateTime:
 
-        time.sleep(1)
+                lastUpdateTime = datetime.datetime.fromtimestamp(updateTime)
+
+                data_int = reader.readData('data_int')
+                data_string = reader.readData('data_string')
+                pts_x = reader.readData('data_xy.x')
+                pts_y = reader.readData('data_xy.y')
+
+                print("------SHARED MEM UPDATED!------")
+                print("[last update] {}".format(lastUpdateTime))
+                print("[data_int] {}".format(data_int))
+                print("[data_string] {}".format(data_string))
+                print("[data_xy] x: {}, y: {}".format(pts_x, pts_y))
+
+                lastestUpdateTime = updateTime
+
+        time.sleep(0.1)
 
     
 
