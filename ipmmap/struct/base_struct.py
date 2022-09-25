0582713ad37d524b9a3a8b5c85b9a8c8ae0f7dad
quag-cactus <49008834+quag-cactus@users.noreply.github.com>
2022-09-25 17:08:23 +0900

    update: Update docs of base structures

--- ipmmap/struct/base_struct.py
+++ ipmmap/struct/base_struct.py
@@ -1,23 +1,17 @@
 
 import ctypes
 
-# プロセス間共有メモリ向けメモリマップ構造体（ベース）
-# サブクラスの検索を容易にするために宣言している空の継承クラスです。
 class BaseMmapStructure(ctypes.Structure):
+    """A base struture to map IPMMAP's shared memory space.
+    This is a empty class to be inherited by your Structure and search it. 
+    """
     pass
 
-# メモリマップ構造体の共通ヘッダ
 class MmapStructureHeader(ctypes.Structure):
+    """A common-header of IPMMAP's shared memory space.
+    """
     _fields_ = (
-        ('header_code', ctypes.c_short),    # MmapMangerの共有メモリデータであることを示すバイト列
-        ('time_stamp', ctypes.c_double),    # タイムスタンプ（作成時・更新時に変更する必要があります）
+        ('header_code', ctypes.c_short),    # Unique bytes of indicating that it is Structure for IPMMAP.
+        ('time_stamp', ctypes.c_double),    # Timestamp, UNIX epoch time.
     )
-
-# 二次元座標構造体
-class Point(ctypes.Structure):
-    _fields_ = (
-        ('x', ctypes.c_double), 
-        ('y', ctypes.c_double),
-    )
-
 
